18ece6712127ebe5e412b53c8ec986520e8fba6a
Jojo Zhao <100546748+qjozhao103@users.noreply.github.com>
2022-09-24 17:51:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// items:
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+
+container.addEventListener("mousemove", (event) => {
+  const xAxis = (window.innerWidth / 2 - event.pageX) / 25;
+  const yAxis = (window.innerHeight / 2 - event.pageY) / 25;
+  card.style.transform = `rotateX(${yAxis}deg) rotateY(${xAxis}deg)`;
+});
+
+// Animate In
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  // Popout
+  title.style.transform = "translateZ(150px)";
+  sneaker.style.transform = "translateZ(200px) rotateZ(-30deg)";
+  description.style.transform = "translateZ(125px)";
+  sizes.style.transform = "translateZ(100px)";
+  purchase.style.transform = "translateZ(75px)";
+});
+
+// Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
+});
